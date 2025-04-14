9514c39df9a44dd2b7d90319b3ccfd40b1bc1288
NavarroWD <navarrowalterd@gmail.com>
2025-04-14 03:38:43 -0300

    seg commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola Mundo");
+    // printf("Hola Mundo");
+    int variable = 3;
+    int *punt;
+
+    punt = &variable;
+
+    printf("El contenido del puntero: %d \n", *punt);
+    printf("La direccion de memoria almacenada del puntero: %p \n", punt);
+    printf("La direccion de memoria de la variable: %p \n", &variable);
+    printf("Direccion de memoria del puntero: %p \n", &punt);
+    printf("El tamaño de memoria: %d \n", sizeof(variable));
 }
